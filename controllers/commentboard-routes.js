5f4c9f97f63a422c86eaed5040745ed04ca886f8
glanctot <gregory.lanctot28@gmail.com>
2022-01-23 13:38:58 -0600

    capturing form data

--- controllers/commentboard-routes.js
+++ controllers/commentboard-routes.js
@@ -3,7 +3,25 @@ const sequelize = require('../config/connection');
 const { Comment, User, Vote } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('commentboard')
+    Comment.findAll({
+        attributes: [
+            'id', 'drink_name', 'drink_description', 'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbCommentData => {
+        const comments = dbCommentData.map(comment => comment.get({ plain: true }));
+        res.render('commentboard', { comments });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
 
 module.exports = router;
