3a972e2b8bcd9d959dc09f66f2b3edaf715f96d5
Shaun S <shaun.sticka@outlook.com>
2022-01-23 21:45:03 -0600

    replaced icons and half done with functionality code

--- public/javascript/random-options.js
+++ public/javascript/random-options.js
@@ -4,21 +4,62 @@ const include = ["a splash of", "a few drops of", "a shot of", "a hint of", "mix
 const mixer = ["root beer", "coke", "sprite", "water", "OJ", "Dr. Pepper", "pineapple juice", "tomato juice", "tonic water", "lemonade", "carbonated water"];
 const topping = ["salt", "pepper", "tobasco", "lemon", "lime", "sugar", "berries", "mint", "cinnamon"];
 
-// return random item from the arrays
+// return random item from the arrays to the page
 
-function getAlcTypes(alcTypes) {
-    return alcTypes[Math.floor(Math.random()*alcTypes.length)]
+function lowDrunk() {
+    var display = document.querySelector("#random-one")
+    var randomOne = document.createElement("div")
+    var randomDrink = Math.floor(Math.random()*alcTypes.length)
+    var firstVariable = alcTypes[randomDrink]
+    var drinkText = document.createElement("p")
+
+    drinkText.textContent = ("Made with " + firstVariable)
+
+    display.appendChild(randomOne).appendChild(drinkText)
 };
 
-function getInclude(include) {
-    return alcTypes[Math.floor(Math.random()*include.length)]
+function midDrunk() {
+    var randomDrink = Math.floor(Math.random()*alcTypes.length)
+    var randomDrinkAlt = Math.floor(Math.random()*alcTypes.length)
+    var firstVariable = alcTypes[randomDrink]
+    var firstVariableAlt = alcTypes[randomDrinkAlt]
+    var display = document.querySelector("#random-one")
+    var randomTwo = document.createElement("div")
+    var randomInclude = Math.floor(Math.random()*include.length)
+    var secondVariable = include[randomInclude]
+    var includeText = document.createElement("p")
+
+    includeText.textContent = ("Made with " + firstVariable + " and include " + secondVariable + " " + firstVariableAlt)
+
+    display.appendChild(randomTwo).appendChild(includeText)
 };
 
-function getMixer(mixer) {
-    return alcTypes[Math.floor(Math.random()*mixer.length)]
-};
+// function highDrunk() {
+//     var display = document.querySelector("#random-one")
+//     var randomOne = document.createElement("div")
+//     var randomDrink = Math.floor(Math.random()*alcTypes.length)
+//     var firstVariable = alcTypes[randomDrink]
+//     var drinkText = document.createElement("p")
 
-function getTopping(topping) {
-    return alcTypes[Math.floor(Math.random()*topping.length)]
-};
+//     drinkText.textContent = firstVariable
 
+//     display.appendChild(randomOne).appendChild(drinkText)
+// };
+
+// function lowDrunk() {
+//     var display = document.querySelector("#random-one")
+//     var randomOne = document.createElement("div")
+//     var randomDrink = Math.floor(Math.random()*alcTypes.length)
+//     var firstVariable = alcTypes[randomDrink]
+//     var drinkText = document.createElement("p")
+
+//     drinkText.textContent = firstVariable
+
+//     display.appendChild(randomOne).appendChild(drinkText)
+// };
+
+
+document.querySelector("#low-drunk").addEventListener("click", lowDrunk);
+document.querySelector("#mid-drunk").addEventListener("click", midDrunk);
+// document.querySelector("#high-drunk").addEventListener("click", randomMixer);
+// document.querySelector("#too-drunk").addEventListener("click", randomTopping);
