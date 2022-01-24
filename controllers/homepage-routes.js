3646c8ea241e49a798df72fa71068e6ddadd01db
glanctot <gregory.lanctot28@gmail.com>
2022-01-24 11:02:19 -0600

    conditionally render login/logout and comments based on session

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -4,7 +4,9 @@ const {Comment, User, Vote } = require("../models");
 
 router.get('/', (req, res) => {
     console.log(req.session);
-    res.render('homepage');
+    res.render('homepage', {
+        loggedIn: req.session.loggedIn
+    });
 })
 
 router.get('/login', (req, res) => {
@@ -13,39 +15,6 @@ router.get('/login', (req, res) => {
         return;
     }
     res.render('login');
-})
-
-router.get('/commentboard/:id', (req, res) => {
-    Comment.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'drink_name',
-            'drink_description',
-            'created_at'
-        ],
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbCommentData => {
-        if (!dbCommentData) {
-            res.status(404).json({ message: 'No comment found with this id' });
-            return;
-        }
-        const comment = dbCommentData.get({ plain: true });
-
-        res.render('commentboard', { comment });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
 })
 
 module.exports = router;
