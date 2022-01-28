9f902faa46d67d377a8d08151399b82c68ea990e
bdurushia <b.durushia@gmail.com>
2022-01-27 19:50:45 -0500

    Restyled the header for mobile, fixed title alignment for comment for, and changed comment input to textarea to contain text and set a min and max char limit to field.

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,7 @@ async function addCommentHandler(event) {
     event.preventDefault();
 
     const drink_name = document.querySelector("input[name='drink-name']").value.trim();
-    const drink_description = document.querySelector("input[name='drink-description']").value.trim();
+    const drink_description = document.querySelector("textarea[name='drink-description']").value.trim();
 
     const response = await fetch ("/api/comments", {
         method: "POST",
